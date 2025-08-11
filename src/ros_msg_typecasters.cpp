daa84caef8bcc67b6f4ce6b4933b6e5c2b2e3cc7
Robert Haschke <rhaschke@techfak.uni-bielefeld.de>
2025-08-11 23:02:33 +0200

    Update copyright statements

--- src/ros_msg_typecasters.cpp
+++ src/ros_msg_typecasters.cpp
@@ -1,7 +1,7 @@
 /*********************************************************************
  * Software License Agreement (BSD License)
  *
- *  Copyright (c) 2021, Robert Haschke
+ *  Copyright (c) 2021, Bielefeld University
  *  All rights reserved.
  *
  *  Redistribution and use in source and binary forms, with or without
